8434086999b64be1a4ac15b5929ef60da35f9628
Oleksii Drenov <leshadrenev@gmail.com>
2025-09-15 00:41:06 +0300

    Change the REST_FRAMEWORK default permission to custom permission class

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -144,6 +144,6 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated"
+        "cinema.permissions.IsAdminOrIfAuthenticatedReadOnly"
     ]
 }
